cb55e17ff641901591ad6fcdb2695cd1d0513e11
jose garcia <reyes2000jose@gmail.com>
2020-02-25 22:45:54 -0500

    updating tools.py

--- tools.py
+++ tools.py
@@ -35,7 +35,30 @@ def retrieve_face_coordinates(path, text_format="cat"):
                     "right-ear-2": (points[15], points[16]),
                     "right-ear-3": (points[17], points[18]),
                    }
+
+    top_left_x = points_coord["left-ear-2"][0]
+    top_left_y = points_coord["left-ear-2"][1]
+    width = points_coord["right-ear-2"][0] - points_coord["left-ear-2"][0]
+    height = points_coord["mouth"][1] - points_coord["left-ear-2"][1]
+    points_coord.update({"top_left_y": top_left_y, "width": width, "top_left_x": top_left_x, "height": height})
     return points_coord
+
+def add_roi_box(image, coordinates, color=(19, 199, 109)):
+    output = image.copy()
+
+    cv2.line(output, (coordinates["top_left_x"], coordinates["top_left_y"]),
+                     (coordinates["top_left_x"] + coordinates["width"], coordinates["top_left_y"]),
+                     (19, 199, 109))
+    cv2.line(output, (coordinates["top_left_x"] + coordinates["width"], coordinates["top_left_y"]),
+                     (coordinates["top_left_x"] + coordinates["width"], coordinates["top_left_y"] + coordinates["height"]),
+                     (19, 199, 109))
+    cv2.line(output, (coordinates["top_left_x"] + coordinates["width"], coordinates["top_left_y"] + coordinates["height"]),
+                     (coordinates["top_left_x"], coordinates["top_left_y"] + coordinates["height"]),
+                     (19, 199, 109))
+    cv2.line(output, (coordinates["top_left_x"], coordinates["top_left_y"] + coordinates["height"]),
+                     (coordinates["top_left_x"], coordinates["top_left_y"]),
+                     (19, 199, 109))
+    return output
 
 def add_facial_landmarks(image, coordinates, color=(19, 199, 109)):
     output = image.copy()
@@ -51,3 +74,34 @@ def add_facial_landmarks(image, coordinates, color=(19, 199, 109)):
     cv2.line(output, coordinates["left-eye"], coordinates["mouth"], (19, 199, 109))
 
     return output
+
+def add_eyes(image, left_eye_coordinates, right_eye_coordinates, radius=2, color=(19, 199, 109)):
+    output = image.copy()
+    output = cv2.circle(output, left_eye_coordinates, radius, color, -1)
+    output = cv2.circle(output, right_eye_coordinates, radius, color, -1)
+    return output
+
+def get_resized_eye_point_position(img_original, resized_image, original_coordinates):
+    original_width = img_original.shape[1]
+    original_height = img_original.shape[0]
+    resized_width = resized_image.shape[1]
+    resized_height = resized_image.shape[0]
+
+    new_x = (original_coordinates[0]/original_width)*resized_width
+    new_y = (original_coordinates[1]/original_height)*resized_height
+
+    new_x = round(new_x)
+    new_y = round(new_y)
+    return (new_x, new_y)
+
+def verify_and_get_valid_roi(rects, left_eye_coordinates, right_eye_coordinates):
+    left_eye_x, left_eye_y = left_eye_coordinates[0], left_eye_coordinates[1]
+    right_eye_x, right_eye_y = right_eye_coordinates[0], right_eye_coordinates[1]
+    for rect in rects:
+        x,y,w,h = int(rect[0]), int(rect[1]), int(rect[2]), int(rect[3])
+        test_1 = left_eye_y < y + h and left_eye_y > y
+        test_2 = left_eye_x > x and left_eye_x < x + w
+        if test_1 and test_2:
+            return (True, (x, y, w, h))
+
+    return (False, (None, None, None, None))
